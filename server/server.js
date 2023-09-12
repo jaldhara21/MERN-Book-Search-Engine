3201a8ea041cb8b4443777ef7edc4156532f730a
jaldhara.patel <jaldharapatel91@gmail.com>
2023-09-12 10:47:17 -0400

    Added authmiddleware, express and GrapgQL server path

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
-const express = require('express');
+const express = require("express");
 // Import the ApolloServer class
-const { ApolloServer } = require('apollo-server-express');
+const { ApolloServer } = require("apollo-server-express");
 // Import the two parts of a GraphQL schema
 const { typeDefs, resolvers } = require("./schemas");
-const path = require('path');
-const db = require('./config/connection');
-const app = express();
+const db = require("./config/connection");
+const path = require("path");
+const { authMiddleware } = require("./utils/auth");
+
 const PORT = process.env.PORT || 3001;
+const app = express();
 
 // Create a new Apollo Server instance with type definitions and resolvers
 const server = new ApolloServer({
@@ -17,13 +19,12 @@ const server = new ApolloServer({
 
 // Apply Apollo Server as middleware to Express app
 server.applyMiddleware({ app });
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
 // Handle all other routes by serving the React app's index.html
@@ -31,7 +32,9 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
-// Open a connection to the MongoDB database
-db.once('open', () => {
+db.once("open", () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log(
+    `GraphQL server ready at http://localhost:${PORT}${server.graphqlPath}`
+  );
 });

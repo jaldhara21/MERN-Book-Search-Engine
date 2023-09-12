9307d50c426e4934981c426106b50370d34ae0fc
jaldhara.patel <jaldharapatel91@gmail.com>
2023-09-12 10:03:12 -0400

    Added query and nutation for adding new user, user login,savebook and removebook

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,77 @@
+const { AuthenticationError } = require("apollo-server-express");
+// import user model
+const { User } = require("../models");
+// import sign token function from auth
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    // Resolver for the "me" query
+    me: async (parent, args, context) => {
+      if (context.user) {
+        // If the user is authenticated, retrieve user data while excluding sensitive fields
+        const userData = await User.findOne({ _id: context.user._id }).select(
+          "-__v -password"
+        );
+        return userData;
+      }
+      // If the user is not authenticated, throw an AuthenticationError
+      throw new AuthenticationError("You need to be logged in!");
+    },
+  },
+
+  Mutation: {
+    // Resolver for adding a new user
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    // Resolver for user login
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("No user found");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    // Resolver for saving a book to a user's profile
+    saveBook: async (parent, { newBook }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { savedBooks: newBook } },
+          { new: true }
+        );
+        return updatedUser;
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+
+    // Resolver for removing a book from a user's profile
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId } } },
+          { new: true }
+        );
+        return updatedUser;
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+  },
+};
+
+module.exports = resolvers;
